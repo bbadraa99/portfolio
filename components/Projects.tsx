c5a58852b7400a82270e7413082c6bc53488ab24
bbadraa99 <bbadraa8@gmail.com>
2024-04-21 17:43:09 +0900

    Fixed some text color issue and some justification

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
     <div id="projects" className="py-10 px-10 pb-10">
       <h1 className="bold-32 center py-10 pb-20 text-white font-dmono">03. Personal Projects</h1>
       <div className="flex flex-col ">
-        <div className="flex flex-col items-center lg:flex-row gap-8  ">
+        <div className="flex flex-col items-center justify-center lg:flex-row gap-8  ">
           <ProjectMaker name={PROJECT_ID[0].name} description={PROJECT_ID[0].description} path={PROJECT_ID[0].path} href={PROJECT_ID[0].href}/>
           <ProjectMaker name={PROJECT_ID[1].name} description={PROJECT_ID[1].description} path={PROJECT_ID[1].path} href={PROJECT_ID[1].href}/>
         </div>
