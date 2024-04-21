c039581c9b16d0a085ffecbba441dec34d7fd6a6
bbadraa99 <bbadraa8@gmail.com>
2024-04-21 17:17:38 +0900

    Fixed null type in Experiences section

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -16,7 +16,7 @@ const Experiences = () => {
           <div className="timeline-middle">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" className="h-7 w-7"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
           </div>
-          <Link href="https://pal.unist.ac.kr/" target='_blank' className={`timeline-start md:text-end mb-10 p-2 cursor-pointer font-mono ${hoveredExperience !== '1' && hoveredExperience !== null ? 'opacity-50' : ''} ${hoveredExperience === '1'  ? 'bg-stone-200 bg-opacity-20 rounded-md' : ''}`}
+          <Link href="https://pal.unist.ac.kr/" target='_blank' className={`timeline-start md:text-end mb-10 p-2 cursor-pointer font-mono ${hoveredExperience !== '1' && hoveredExperience !== 'empty' ? 'opacity-50' : ''} ${hoveredExperience === '1'  ? 'bg-stone-200 bg-opacity-20 rounded-md' : ''}`}
             onMouseEnter={() => setHoveredExperience('1')}
             onMouseLeave={() => setHoveredExperience('empty')}>
             <time className="font-mono italic text-white">2024.03 - Present</time>
@@ -37,7 +37,7 @@ const Experiences = () => {
           <div className="timeline-middle">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" className="h-7 w-7"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
           </div>
-          <Link href="https://www.carcare.mn/" target='_blank' className={`timeline-end mb-10 p-2 cursor-pointer font-mono ${hoveredExperience !== '2' && hoveredExperience !== null ? 'opacity-50' : ''} ${hoveredExperience === '2' ? 'bg-stone-200 bg-opacity-20 rounded-md' : ''}`}
+          <Link href="https://www.carcare.mn/" target='_blank' className={`timeline-end mb-10 p-2 cursor-pointer font-mono ${hoveredExperience !== '2' && hoveredExperience !== 'empty' ? 'opacity-50' : ''} ${hoveredExperience === '2' ? 'bg-stone-200 bg-opacity-20 rounded-md' : ''}`}
             onMouseEnter={() => setHoveredExperience('2')}
             onMouseLeave={() => setHoveredExperience('empty')} >
             <time className="font-mono italic text-white">2024.01 - 2024.02</time>
@@ -57,7 +57,7 @@ const Experiences = () => {
           <div className="timeline-middle">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" className="h-7 w-7"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
           </div>
-          <Link href="https://digitalsolutions.mn/#slide01" target='_blank' className={`timeline-start md:text-end mb-10 p-2 cursor-pointer font-mono ${hoveredExperience !== '3' && hoveredExperience !== null ? 'opacity-50' : ''} ${hoveredExperience === '3'  ? 'bg-stone-200 bg-opacity-20 rounded-md' : ''}`}
+          <Link href="https://digitalsolutions.mn/#slide01" target='_blank' className={`timeline-start md:text-end mb-10 p-2 cursor-pointer font-mono ${hoveredExperience !== '3' && hoveredExperience !== 'empty' ? 'opacity-50' : ''} ${hoveredExperience === '3'  ? 'bg-stone-200 bg-opacity-20 rounded-md' : ''}`}
             onMouseEnter={() => setHoveredExperience('3')}
             onMouseLeave={() => setHoveredExperience('empty')}>
             <time className="font-mono italic text-white">2023.01 - 2023.02</time>
