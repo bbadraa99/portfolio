909353fcfedb9e4c6240860e119e3a7efe91c4c5
bbadraa99 <bbadraa8@gmail.com>
2024-04-22 11:49:33 +0900

    Added icon and fixed some letter issue

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
   
   return (
     <div id="projects" className="py-10 px-4 lg:px-10 pb-10">
-      <h1 className="bold-28 flex justify-end items-end py-10 pb-10 lg:pb-20 text-white font-dmono">03.&nbsp;<span className="bold-32">Projects</span></h1>
+      <h1 className="bold-28 flex justify-center items-end py-10 pb-10 lg:pb-20 text-white font-dmono">03.&nbsp;<span className="bold-32">Projects</span></h1>
       <div className="flex flex-col ">
         <div className="flex flex-col items-center justify-center lg:flex-row gap-8  ">
           <ProjectMaker name={PROJECT_ID[0].name} description={PROJECT_ID[0].description} path={PROJECT_ID[0].path} href={PROJECT_ID[0].href}/>
